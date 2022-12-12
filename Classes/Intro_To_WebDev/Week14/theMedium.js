a2eef45169f655fb789bd0bcecef0add7def3028
Shugo0016 <93568520+Shugo0016@users.noreply.github.com>
2022-12-12 08:21:14 -0500

    score presented

--- Classes/Intro_To_WebDev/Week14/theMedium.js
+++ Classes/Intro_To_WebDev/Week14/theMedium.js
@@ -4,15 +4,14 @@ var curr_day = date.getDate();
 let curr_month = date.getMonth() + 1;
 let curr_year = date.getFullYear();
 let curr_sport = "hockey";
+let check_user_input = false;
+console.log(check_user_input);
+
 matchTable.append("There are 5 sports to choose from... Hockey, Tennis, Basketball, Cricket, Soccer");
 
 curr_day = curr_day.toString();
 curr_month = curr_month.toString();
 curr_year = curr_year.toString();
-
-input_year = curr_year;
-input_month = curr_month;
-input_day = curr_day;
 
 if(curr_day.length < 2) {
     curr_day = "0" + curr_day;
@@ -39,13 +38,17 @@ if (!e.target.matches('.dropbtn')) {
     }
 }
 
+function get_score(specific_match) {
+    let homeScore = specific_match.Tr1;
+    console.log(homeScore);
+    let awayScore = specific_match.Tr2; 
+    console.log(awayScore);
+    return homeScore + " - " + awayScore;
+}
 
-
-// Adds current playing teams to webpage
+// Adds planned matches to screen
 function addMatchTile(date, match){
     //createing the tile div
-
-    matchTable.append("Today's " + curr_sport + " Games:");
 
     var matchtile = document.createElement('div');
 
@@ -81,8 +84,27 @@ function addMatchTile(date, match){
     var game_date = document.createElement('p');
     game_date.innerHTML = date;
 
+    const today = new Date();
     var score = document.createElement('p');
-    score.innerHTML = "5 - 4";
+    let value = ""
+    let today_day = today.getDate()
+    let today_month = today.getMonth() + 1;
+    let today_year = today.getFullYear();
+    today_day = today_day.toString();
+
+    today_month = today_month.toString();
+
+    today_year= today_year.toString();
+
+ 
+    if(curr_day == today_day && curr_month == today_month && curr_year == today_year) {
+        value = "Game Hasn't started"
+    }
+    else {
+        value = get_score(match);
+    }
+    score.innerHTML = value;
+    
 
 
     //append all the element to the parent
@@ -108,11 +130,13 @@ const options = {
 
 // Gets user input and stores it in variables
 function othername() {
-    var input_year = document.getElementById("userInput1").value;
+    curr_year = document.getElementById("userInput1").value;
 
-    var input_month = document.getElementById("userInput2").value;
+    curr_month = document.getElementById("userInput2").value;
 
-    var input_day = document.getElementById("userInput3").value;
+    curr_day = document.getElementById("userInput3").value;
+    check_user_input = true;
+    switch_sports()
 }
 
 // Switches sport based on selected button in the drop down menu
@@ -121,56 +145,63 @@ function switch_sports(sport_name) {
     while (matchTable.firstChild) {
         matchTable.removeChild(matchTable.firstChild);
     }
-    // console.log(curr_sport);
 
+// Pareses date into into usable string 
+function get_date(matchTime) {
+    gameTime = matchTime.toString();
+
+    var year = "";
+    var month = "";
+    var day = "";
+    var hour = "";
+    var min = "";
+    var gameDay = "";
+
+    // Breaking weird number that represents the date representing date into pieces
+    for(var j = 0; j < 12; j++) {
+        if (j >= 0 && j < 4) {
+            year += gameTime[j];
+        }
+        else if(j >= 4 && j < 6) {
+            month += gameTime[j];
+        }
+        else if(j >= 6 && j < 8) {
+            day += gameTime[j];
+        }
+        else if(j >= 8 && j < 10) {
+            hour += gameTime[j];
+        }
+        else if(j >= 10 && j < 12) {
+            min += gameTime[j];
+        }
+    }
+    gameDay = "Date: " + month + "/" + day + "/" + year + " Time: " + hour + ":" + min;
+    return gameDay;
+}
+
+// Fetching data from livescore api and storing most important values in variables
     fetch('https://livescore6.p.rapidapi.com/matches/v2/list-by-date?Category=' + curr_sport +'&Date=' + curr_year + curr_month + curr_day + '&Timezone=-7', options)
 	.then(response => {
         return response.json()
     })
 	.then((data) => {
         var todays_games = data.Stages[0].Events;
-        console.log("DATA is: ", todays_games);
+        // console.log("DATA is: ", todays_games);
 
         for (var i = 0; i < todays_games.length; i++) {
 
-            
-            var gameTime = todays_games[i].Esd;
             var team1 = todays_games[i].T1[0].Nm;
             var team2 = todays_games[i].T2[0].Nm;
-            gameTime = gameTime.toString();
 
-            var year = "";
-            var month = "";
-            var day = "";
-            var hour = "";
-            var min = "";
-            var gameDay = "";
-
-
-            // Breaking weird number that represents the date representing date into pieces
-            for(var j = 0; j < 12; j++) {
-                if (j >= 0 && j < 4) {
-                    year += gameTime[j];
-                }
-                else if(j >= 4 && j < 6) {
-                    month += gameTime[j];
-                }
-                else if(j >= 6 && j < 8) {
-                    day += gameTime[j];
-                }
-                else if(j >= 8 && j < 10) {
-                    hour += gameTime[j];
-                }
-                else if(j >= 10 && j < 12) {
-                    min += gameTime[j];
-                }
+            var gameTime = todays_games[i].Esd;
+            console.log(gameTime);
+            let gameDay = "";
+            if (check_user_input != true) {
+                gameDay = get_date(gameTime);
             }
-
-            gameDay = "Date: " + month + "/" + day + "/" + year + " Time: " + hour + ":" + min;
-
-            console.log(team1);
-            console.log(team2);
-            console.log(gameDay);
+            else {
+                gameDay = "Date: " + curr_month + "/" + curr_day + "/" + curr_year;
+            }
 
             addMatchTile(gameDay,todays_games[i]);
         }
